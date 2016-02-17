ada7f0cd15e6e3c4e8b99e0123840ea178012a27
Alexander Farley <crimsond@udel.edu>
2016-02-17 15:20:19 -0500

    initialized variables as 0 for lack of how to use an input

--- Project-1/src/MainPackage/Main.java
+++ Project-1/src/MainPackage/Main.java
@@ -1,10 +1,17 @@
 package MainPackage;
+import java.util.Scanner;   //not sure about this, thought we needed in order to use input function
 
-public class Main {
+	public class Main {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int yrs2wrk = 0;                  //initializing as 0 for place holders
+		int rtrnrate = 0;
+		int yrsinret = 0;
+		int retrtrnrate = 0;
+		int reqincome = 0;
+		int mnthlySSI = 0 ;
+		
 	}
 
 }
