6591c6e7cf971a119d6dd14797d7cf7fedc2a881
Alexander Farley <crimsond@udel.edu>
2016-02-17 15:49:25 -0500

    figured out how to use inputs an print fuctions. See code for details

--- Project-1/src/MainPackage/Main.java
+++ Project-1/src/MainPackage/Main.java
@@ -5,12 +5,16 @@ import java.util.Scanner;   //not sure about this, thought we needed in order to
 
 		public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int yrs2wrk = 0;                  //initializing as 0 for place holders
-		int rtrnrate = 0;
-		int yrsinret = 0;
-		int retrtrnrate = 0;
-		int reqincome = 0;
-		int mnthlySSI = 0 ;
+			Scanner input = new Scanner(System.in);  // can now create input statements
+			System.out.println("enter a number"); //test statement 
+			int workweek = input.nextInt(); // assigns an inputed number to workweek
+			System.out.println("you entered:" + workweek);
+			int yrs2wrk = 0;                  //initializing as 0 for place holders
+			int rtrnrate = 0;
+			int yrsinret = 0;
+			int retrtrnrate = 0;
+			int reqincome = 0;
+			int mnthlySSI = 0 ;
 		
 	}
 
